4a6183fd17a52d975b21db7697342498cd24c9de
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-02-14 17:13:12 -0500

    added VM version checks

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -35,10 +35,23 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	// This function would fail the test if there is an error.
 	nic := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
 
+	//get the virtual machine image
+	vmImage := azure.GetVirtualMachineImage(t, resourceGroup, vmName)
+
+
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
 	//confirm NIC exists 
 	assert.NotNil(t, nic)
 	//confirm nic connection to vm
-	assert.True(t, len(nic) == 1, "There is one NIC attached to VM")
+	assert.True(t, len(nic) == 1, "There should be one NIC attached to VM")
+	fmt.Println(nic)
+
+	//confirm that the image exists
+	assert.NotNil(t, vmImage, "VM image should be found.")
+	//confirm that the it is running an Ubuntu version
+	assert.Equal(t, "Canonical", vmImage.Publisher, "VM image publisher should be 'Canonical'")
+	assert.Equal(t, "0001-com-ubuntu-server-jammy", vmImage.Offer, "VM image offer should be '0001-com-ubuntu-server-jammy'")
+	assert.Equal(t, "22_04-lts-gen2", vmImage.Sku, "VM image SKU should be '22_04-lts-gen2'")
+
 }
