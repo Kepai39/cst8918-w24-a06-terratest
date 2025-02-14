8c344ead6f26c6fc482799af1f5d6a9d1f8dbdfa
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-02-14 17:19:11 -0500

    completed testing for ubuntu now fixing

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -36,7 +36,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	nic := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
 
 	//get the virtual machine image
-	vmImage := azure.GetVirtualMachineImage(t, resourceGroup, vmName)
+	vmImage := azure.GetVirtualMachineImage(t, resourceGroupName, vmName, subscriptionID)
 
 
 	// Confirm VM exists
@@ -45,13 +45,12 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	assert.NotNil(t, nic)
 	//confirm nic connection to vm
 	assert.True(t, len(nic) == 1, "There should be one NIC attached to VM")
-	fmt.Println(nic)
 
 	//confirm that the image exists
 	assert.NotNil(t, vmImage, "VM image should be found.")
 	//confirm that the it is running an Ubuntu version
 	assert.Equal(t, "Canonical", vmImage.Publisher, "VM image publisher should be 'Canonical'")
 	assert.Equal(t, "0001-com-ubuntu-server-jammy", vmImage.Offer, "VM image offer should be '0001-com-ubuntu-server-jammy'")
-	assert.Equal(t, "22_04-lts-gen2", vmImage.Sku, "VM image SKU should be '22_04-lts-gen2'")
+	assert.Equal(t, "22_04-lts-gen2", vmImage.SKU, "VM image SKU should be '22_04-lts-gen2'")
 
 }
